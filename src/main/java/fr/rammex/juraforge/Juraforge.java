d5669477696b4b79dfa3aba09e1d77aa0c790e2a
romai <romain.kaspar@gmail.com>
2024-09-10 20:56:01 +0200

    base

---
+++ src/main/java/fr/rammex/juraforge/Juraforge.java
@@ -0,0 +1,18 @@
+package fr.rammex.juraforge;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Juraforge extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+        
+    }
+}
