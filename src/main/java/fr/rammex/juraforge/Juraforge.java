1fd6901f5a0c3fc2708d6bac7ed9734d63d3928c
romai <romain.kaspar@gmail.com>
2024-09-10 21:03:15 +0200

    implémentation AuraSkills

--- src/main/java/fr/rammex/juraforge/Juraforge.java
+++ src/main/java/fr/rammex/juraforge/Juraforge.java
@@ -1,5 +1,6 @@
 package fr.rammex.juraforge;
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Juraforge extends JavaPlugin {
@@ -7,12 +8,23 @@ public final class Juraforge extends JavaPlugin {
     @Override
     public void onEnable() {
 
-
+        // Check if AuraSkill plugin is installed
+        checkAuraSkill();
     }
 
     @Override
     public void onDisable() {
 
         
+    }
+
+    public void checkAuraSkill() {
+        Plugin auraSkill = getServer().getPluginManager().getPlugin("AuraSkills");
+        if (auraSkill == null) {
+            getLogger().severe("[☓] AuraSkill plugin pas installé! Désactivation de JuraForge...");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+        getLogger().info("[✓] AuraSkill plugin trouvé! Juraforge activé.");
     }
 }
